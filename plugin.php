d3be0132a94f32480dfed02bae2b60227a2e2fa5
mjansenDatabay <m1cha3l>
2014-12-05 14:40:06 +0100

    4.5.x vs. 5.0.x

--- plugin.php
+++ plugin.php
@@ -3,12 +3,12 @@
 $id = 'roundcubeembed';
 
 // code version; must be changed for all code changes
-$version = '1.1.0';
+$version = '1.1.1';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = '4.2.0';
-$ilias_max_version = '4.5.999';
+$ilias_max_version = '5.0.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Michael Jansen';
